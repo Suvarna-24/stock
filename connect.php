e1fe6c1635dafdef7078034c0d3f02507865d8e2
Suvarna-24 <47240328+Suvarna-24@users.noreply.github.com>
2021-07-02 00:25:08 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+//$con=mysqli_connect("localhost","root","","stock");
+$con=mysqli_connect("sql6.freesqldatabase.com","sql6422177","dKNFHXkf6Z","sql6422177");
+?>
+<?php
+//$con=mysqli_connect("sql6.freesqldatabase.com","sql6422177","dKNFHXkf6Z","sql6422177");
+/* $servername = "sql6.freesqldatabase.com";
+$username = "sql6422177";
+$password = "dKNFHXkf6Z"; */
+
+// Create connection
+//$con = new mysqli($servername, $username, $password);
+//$con = mysqli_connect($servername, $username, $password);
+// Check connection
+//if ($con->connect_error) {
+ // die("Connection failed: " . $con->connect_error);
+//}
+//echo "Connected successfully";
+?>
